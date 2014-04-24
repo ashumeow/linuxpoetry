27303a5c6f3772d4fd1a90e002b94ee81b6091c1
Morgan Phillips <morgan@artistgrowth.com>
2014-04-23 22:11:10 -0500

    Adds a new poem about errno.h

--- deployment/files/scripts/start_gunicorn.sh
+++ deployment/files/scripts/start_gunicorn.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 export PYTHONPATH=$PYTHONPATH:/var/projects/linuxpoetry/code
-/var/projects/linuxpoetry/env/bin/gunicorn poetry:application -b 0.0.0.0:80
+/var/projects/linuxpoetry/env/bin/gunicorn poetry.wsgi:application -b 0.0.0.0:80
