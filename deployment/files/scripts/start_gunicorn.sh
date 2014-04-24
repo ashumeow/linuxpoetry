5a56e7e31402cacdbeabf6e8d11956ebdccb91c8
Morgan Phillips <morgan@artistgrowth.com>
2014-04-23 19:00:44 -0500

    Settings changes.

--- deployment/files/scripts/start_gunicorn.sh
+++ deployment/files/scripts/start_gunicorn.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-cd /var/projects/linuxpoetry/code
-../env/bin/gunicorn poetry:application -b 0.0.0.0:80
+export PYTHONPATH=$PYTHONPATH:/var/projects/linuxpoetry/code
+/var/projects/linuxpoetry/env/bin/gunicorn poetry:application -b 0.0.0.0:80
